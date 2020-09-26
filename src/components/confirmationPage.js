6d64581365936ae4820e15c2fc87f729438ed71b
yisacc <yisacc.abmyfather@gmail.com>
2020-09-26 12:29:14 +0300

    confirmation page finalized

--- src/components/confirmationPage.js
+++ src/components/confirmationPage.js
@@ -8,6 +8,9 @@ import {
   MDBCardText,
   MDBCol,
   MDBRow,
+  MDBTable,
+  MDBTableHead,
+  MDBTableBody,
 } from 'mdbreact';
 function confirmationPage() {
   const handlePrint = () => {
@@ -31,7 +34,7 @@ function confirmationPage() {
     var pri = document.getElementById('ifmcontentstoprint').contentWindow;
     pri.document.open();
     pri.document.write(
-      '<html><head><title>Print</title><link media="all" type="text/css" rel="stylesheet" href="./App.css"/>'
+      '<html><head><title>Print</title><link media="all" type="text/css" rel="stylesheet" href="../App.css"/>'
     );
     pri.document.write('</head><body >');
     pri.document.write(content.innerHTML);
@@ -40,32 +43,20 @@ function confirmationPage() {
     pri.focus();
     pri.print();
   };
-  const handleDownload = () => {
-    debugger;
-    var content = document.getElementById('divcontents');
-    var pri = document.getElementById('ifmcontentstoprint').contentWindow;
-    pri.document.open();
-    pri.document.write(content.innerHTML);
-    pri.document.close();
-    pri.focus();
-    pri.print();
-  };
+
   return (
     <>
-      <MDBContainer
-        className="passport-container confirmation-page pt-3"
-        id="divcontents"
-        fluid
-      >
+      <MDBContainer className="passport-container confirmation-page pt-3" fluid>
         <MDBCol>
           <MDBCard>
-            <MDBCardBody>
+            <MDBRow className="confirmation-print-btn no-print">
+              <button class="print-button " onClick={() => window.print()}>
+                <span class="pr-2">Print</span>
+                <span class="print-icon"></span>
+              </button>
+            </MDBRow>
+            <MDBCardBody id="section-to-print">
               <MDBCardTitle className="confirmation-page-title1">
-                <MDBRow class="confirmation-print-btn">
-                  <button class="print-button" onClick={() => handlePrint()}>
-                    <span class="print-icon"></span>
-                  </button>
-                </MDBRow>
                 <MDBRow>
                   <i class="fas fa-check-circle fa-3x confirmation-page-icon"></i>
                 </MDBRow>
@@ -96,11 +87,11 @@ function confirmationPage() {
                         <hr class="text-primary" />
                         <div class="form-group form-inline">
                           <label class="control-label col-sm-4 p-0 pr-2 justify-content-end">
-                            Office
+                            Request Status
                           </label>
                           &nbsp;&nbsp;&nbsp;&nbsp;
                           <b>
-                            <label class="font-weight-bold">Ethiopia</label>
+                            <label class="font-weight-bold">Pending</label>
                           </b>
                         </div>
                         <div class="form-group form-inline">
@@ -148,7 +139,7 @@ function confirmationPage() {
                           </div>
                           <div class="form-group form-inline">
                             <label class="control-label col-sm-4 p-0 pr-2 justify-content-end">
-                              Region
+                              Office
                             </label>
                             &nbsp;&nbsp;&nbsp;&nbsp;
                             <b>
@@ -164,14 +155,38 @@ function confirmationPage() {
                 </div>
                 <div class="pt-3">
                   <MDBRow>
-                    <legend class="text-primary">Appointment Details</legend>
+                    <legend class="text-primary">Applicants Details</legend>
                   </MDBRow>
-                  <div class="row">
-                    <fieldset>
-                      <legend>Appointment Site</legend>
-                      <hr class="text-primary" />
-                    </fieldset>
-                  </div>
+                  <MDBTable responsive>
+                    <MDBTableHead color="primary-color" textWhite>
+                      <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Application Id</th>
+                      </tr>
+                    </MDBTableHead>
+                    <MDBTableBody>
+                      <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                      </tr>
+                      <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                      </tr>
+                      <tr>
+                        <td>3</td>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                      </tr>
+                    </MDBTableBody>
+                  </MDBTable>
                 </div>
               </MDBCardText>
             </MDBCardBody>
@@ -179,6 +194,7 @@ function confirmationPage() {
         </MDBCol>
       </MDBContainer>
       <iframe id="ifmcontentstoprint"></iframe>
+      <style>{`@media print {.no-print{display: none;}}`}</style>
     </>
   );
 }
