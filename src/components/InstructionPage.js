0bf7ad2b35f9d38d8081a55190734d07ae550927
Zelalem <zelalembe@ethiopianairlines.com>
2020-09-27 18:27:48 +0300

    Instruction page ui is done

--- src/components/InstructionPage.js
+++ src/components/InstructionPage.js
@@ -5,6 +5,7 @@ import {
 } from "mdbreact";
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
+import { Alert } from 'react-bootstrap';
 
 function InstructionPage() {
   const [requestSubmited, setRequestSubmited] = useState(false);
@@ -33,7 +34,7 @@ function InstructionPage() {
       city: "Addis Ababa",
       country: "Ethiopia",
       channel: "Mobile",
-      paymentOptionsId: selectedOption ? selectedOption.optionId : 0,
+      paymentOptionsId: 2,
       traceNumbers: "",
       Status: 4,
       OrderId: "",
@@ -42,6 +43,7 @@ function InstructionPage() {
     };
     axios.post("https://epassportservices.azurewebsites.net/Payment/api/V1.0/Payment/OrderRequest", body, config)
       .then((todo) => {
+    
         console.log(todo.data)
         setStatus(todo.data.status)
         setInstruction(todo.data.instruction)
@@ -50,131 +52,87 @@ function InstructionPage() {
         setRequestSubmited(true)
       })
       .catch((err) => {
+
         console.log("AXIOS ERROR: ", err.response);
       })
-}, [])
+  }, [])
 
   return (
     <>
-      <MDBContainer className="passport-container confirmation-page pt-3" fluid>
-        <MDBCol>
-          <MDBCard>
-            <MDBRow className="confirmation-print-btn no-print">
-              <button class="print-button " onClick={() => window.print()}>
-                <span class="pr-2">Print</span>
-                <span class="print-icon"></span>
-              </button>
-            </MDBRow>
-            <MDBCardBody id="section-to-print">
-              <MDBCardTitle className="confirmation-page-title1">
-                <MDBRow>
-                  <i class="fas fa-check-circle fa-3x confirmation-page-icon"></i>
-                </MDBRow>
-                <MDBRow>
-                  <h3 class="heading-secondary">
-                    We have recieved your Request
-                  </h3>
-                </MDBRow>
-                <MDBRow>
-                  <h3>Appointment Id 5445568</h3>
-                </MDBRow>
-                <MDBRow>
-                  <p class="font-italic confirmation-page-paragraph">
-                    We will send you an email in 2 hours about your request. If
-                    accepted you will proceed to payment from the website.
-                  </p>
-                </MDBRow>
-              </MDBCardTitle>
-              <MDBCardText className="pl-4">
-                <div class="pt-3">
-                  <MDBRow>
-                    <legend class="text-primary">Appointment Details</legend>
-                  </MDBRow>
-                  <div class="row">
-                    <div class="col-md-6">
-                      <fieldset>
-                        <legend>Appointment Site</legend>
-                        <hr class="text-primary" />
-                        <div class="form-group form-inline">
-                          <label class="control-label col-sm-4 p-0 pr-2 justify-content-end">
-                            Request Status
-                          </label>
-                          &nbsp;&nbsp;&nbsp;&nbsp;
-                          <b>
-                            <label class="font-weight-bold">Pending</label>
-                          </b>
-                        </div>
-                        <div class="form-group form-inline">
-                          <label class="control-label col-sm-4 p-0 pr-2 justify-content-end">
-                            Request Type
-                          </label>
-                          &nbsp;&nbsp;&nbsp;&nbsp;
-                          <b>
-                            <label class="font-weight-bold">New</label>
-                          </b>
-                        </div>
-                        <div class="form-group form-inline">
-                          <label class="control-label col-sm-4 p-0 pr-2 justify-content-end">
-                            Request Mode
-                          </label>
-                          &nbsp;&nbsp;&nbsp;&nbsp;
-                          <b>
-                            <label class="font-weight-bold">Urgent</label>
-                          </b>
-                        </div>
-                      </fieldset>
-                    </div>
-                    <div class="col-md-6">
-                      <fieldset>
-                        <legend>Appointment Site</legend>
-                        <hr class="text-primary" />
-                        <div class="col-md-6">
-                          <div class="form-group form-inline">
-                            <label class="control-label col-sm-4 p-0 pr-2 justify-content-end">
-                              Appointment Date
-                            </label>
-                            &nbsp;&nbsp;&nbsp;&nbsp;
-                            <b>
-                              <label class="font-weight-bold">2020-09-26</label>
-                            </b>
-                          </div>
-                          <div class="form-group form-inline">
-                            <label class="control-label col-sm-4 p-0 pr-2 justify-content-end">
-                              Appointment Time
-                            </label>
-                            &nbsp;&nbsp;&nbsp;&nbsp;
-                            <b>
-                              <label class="font-weight-bold">11:00 Am</label>
-                            </b>
-                          </div>
-                          <div class="form-group form-inline">
-                            <label class="control-label col-sm-4 p-0 pr-2 justify-content-end">
-                              Office
-                            </label>
-                            &nbsp;&nbsp;&nbsp;&nbsp;
-                            <b>
-                              <label class="font-weight-bold">
-                                Addis Ababa
-                              </label>
-                            </b>
-                          </div>
-                        </div>
-                      </fieldset>
-                    </div>
-                  </div>
+      <div id="share-section" class="bg-light text-muted py-5">
+        <div class="container">
+          <div class="row">
+
+            <div class="col-md-8">
+              <div class="u-center-text u-margin-bottom-big"><h2 class="heading-secondary">Payment Instruction</h2></div>
+
+              <h5 class="font-weight-bold u-center-text m3-5">Please follow step below to process payment</h5>
+              <div class="alert alert-success my-4 p-3" role="alert">
+                  Thank you for making this request. Please visit the website to follow the status.
+              </div>
+              <div class="d-flex">
+
+                <div class="p-3 align-self-start">
+                  <i class="fas fa-check fa-1x"></i>
                 </div>
-                <div class="pt-3">
-                  <MDBRow>
-                    <legend class="text-primary">Applicants Details</legend>
-                  </MDBRow>
-               </div>
-              </MDBCardText>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBContainer>
-      <iframe id="ifmcontentstoprint"></iframe>
-      <style>{`@media print {.no-print{display: none;}}`}</style>
+                <div class="p-3 align-self-end">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio iusto, perspiciatis mollitia natus harum?
+            </div>
+              </div>
+
+              <div class="d-flex">
+                <div class="p-3 align-self-start">
+                  <i class="fas fa-check fa-1x"></i>
+                </div>
+                <div class="p-3 align-self-end">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio iusto, perspiciatis mollitia natus harum?
+            </div>
+              </div>
+
+              <div class="d-flex">
+                <div class="p-3 align-self-start">
+                  <i class="fas fa-check fa-1x"></i>
+                </div>
+                <div class="p-3 align-self-end">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio iusto, perspiciatis mollitia natus harum?
+            </div>
+              </div>
+
+              <p class="">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio iusto, perspiciatis mollitia natus harum?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio iusto, perspiciatis mollitia natus harum?
+
+          </p>
+
+              <p class="">
+                N.B : Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio iusto, perspiciatis mollitia natus harum?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio iusto, perspiciatis mollitia natus harum?
+
+          </p>
+
+            </div>
+            <div class="col-md-4 order-md-2 mb-4 mt-5">
+       
+              <ul class="list-group mb-3">
+              <li class="list-group-item ePassprt-color"><h5>Pricing Information</h5></li>
+                <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                    <h6 class="my-0">Selected Payment Option</h6>
+                  </div><span class="text-muted">CBE</span></li>
+                <li class="list-group-item d-flex justify-content-between lh-condensed"><div><h6 class="my-0">Order code</h6>
+                </div><span class="text-muted">DSFDDFE</span></li><li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                    <h6 class="my-0">Page quantity</h6>
+                  </div><span class="text-muted">32</span>
+                </li><li class="list-group-item d-flex justify-content-between">
+                  <span>Amount</span>
+                  <strong>600</strong>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
