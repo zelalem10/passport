05306a7c25ca4699fa8e64f70e9a63cdd2533da0
Atalay Tilahun <atehun@gmail.com>
2020-10-06 11:10:44 +0300

    Pricing detail completed

--- src/components/Payment/PricingDetail.js
+++ src/components/Payment/PricingDetail.js
@@ -23,82 +23,72 @@ const BasicTable = () => {
   }
   const requestInfo = counter.request[counter.request.length-1];
   let requestId=requestInfo?requestInfo.requestId:0;
-  useEffect(() => {
-    API.get("https://epassportservices.azurewebsites.net/Master/api/V1.0/ServicePrice/GetPriceForRequest?requestId="+requestId, config)
-        .then((todo) => 
-        {
-            setTotalPriceList(todo.data.priceTotalDetail);
-            setIndividualPrice(todo.data.individualPrice);
-            setTotalPrice(todo.data.totalPrice);
-        }
-        )
-        .catch((err) => {
-            console.log("AXIOS ERROR: ", err.response);
-        })
-}, [])
+  API.get("https://epassportservices.azurewebsites.net/Master/api/V1.0/ServicePrice/GetPriceForRequest?requestId="+requestId, config)
+  .then((todo) => 
+  {
+      setTotalPriceList(todo.data.priceTotalDetail);
+      setIndividualPrice(todo.data.individualPrice);
+      setTotalPrice(todo.data.totalPrice);
+  }
+  )
+  .catch((err) => {
+      console.log("AXIOS ERROR: ", err.response);
+  })
     return (
         <MDBContainer>
-
             <MDBTable hover>
-                <MDBTableHead>
-                    {/* <tr>
-          <th>First</th>
-          <th>Last</th>
-          <th>Handle</th>
-        </tr> */}
-                </MDBTableHead>
                 <MDBTableBody>
                     {totalPriceList.map((totalPrice) =>
                         <tr>
                             <td><h6 className="font-weight-bolder text-muted text-uppercase"> {totalPrice.lable}</h6></td>
+                            <td><h6 className="font-weight-bolder text-muted text-uppercase"></h6></td>
                             <td><h6 className="font-weight-bolder text-muted text-uppercase"> {totalPrice.amount}</h6></td>
                         </tr>
-
                     )}
-                    
                     <tr>
                         <td><h6 className="font-weight-bold text-danger"> Total</h6> </td>
-                    <td><h6 className="font-weight-bold text-danger"> {totalPrice} ETB</h6> </td>
+                        <td><h6 className="font-weight-bold text-danger"> {totalPrice}</h6> </td>
+                        <td><h6 className="font-weight-bold text-danger"> ETB</h6> </td>
                     </tr>
                 </MDBTableBody>
             </MDBTable>
-            {individualPrice.length>0?(
+            {individualPrice.length > 0 ? (
                 <MDBCard>
                     <MDBRow>
-                <MDBCol md="8"></MDBCol>
-                <MDBCol>
-                    <MDBBtn
-                        color="primary"
-                        onClick={toggleCollapse}
-                        style={{ marginBottom: "1rem" }}
-                    >
-                        View Detail<MDBIcon icon="angle-down" />
-                    </MDBBtn>
-                </MDBCol>
-            </MDBRow>
-            <MDBCollapse id="basicCollapse" isOpen={isOppened}>
-                {individualPrice.map((prices) =>
-                    <MDBListGroup>
-                        <MDBListGroupItem href="#" active>Applicant 1</MDBListGroupItem>
-                        <MDBTable hover>
-                            <MDBTableHead>
-                            </MDBTableHead>
-                            {prices.priceDetail.map((detail) =>
-                                <MDBTableBody>
-                                    <tr>
-                                        <td><h6 className="font-weight-bolder text-muted text-uppercase"> {detail.lable}</h6></td>
-                                        <td><h6 className="font-weight-bolder text-muted text-uppercase"> {detail.amount}</h6></td>
-                                    </tr>
-                                </MDBTableBody>
-                            )}
-                        </MDBTable>
-                    </MDBListGroup>
+                        <MDBCol md="2"></MDBCol>
+                        <MDBCol md="10">
+                            <MDBBtn
+                                color="primary"
+                                onClick={toggleCollapse}
+                                style={{ marginBottom: "1rem" }}
+                                className='px-3'
+                            >
+                                View Detail{isOppened===true?(<MDBIcon icon="angle-up" className='ml-5' />):(<MDBIcon icon="angle-down" className='ml-5' />)}
+                            </MDBBtn>
+                        </MDBCol>
+                    </MDBRow>
+                    <MDBCollapse id="basicCollapse" isOpen={isOppened}>
+                        {individualPrice.map((prices) =>
+                            <MDBListGroup>
+                                <MDBListGroupItem href="#" active>{prices.personFullName}</MDBListGroupItem>
+                                <MDBTable hover>
+                                    <MDBTableHead>
+                                    </MDBTableHead>
+                                    {prices.priceDetail.map((detail) =>
+                                        <MDBTableBody>
+                                            <tr>
+                                                <td><p className="font-weight-bolder text-muted text-uppercase"> {detail.lable}</p></td>
+                                                <td><p className="font-weight-bolder text-muted text-uppercase"> {detail.amount}</p></td>
+                                            </tr>
+                                        </MDBTableBody>
+                                    )}
+                                </MDBTable>
+                            </MDBListGroup>
 
-                )}
-            </MDBCollapse>
+                        )}
+                    </MDBCollapse>
                 </MDBCard>
-            ):(null)}
-            
+            ) : (null)}  
         </MDBContainer>
     );
 }
