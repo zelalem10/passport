cb0387255d472fb0fcf653ba92fbc1d3ba305629
Atalay Tilahun <atehun@gmail.com>
2020-10-08 11:11:50 +0300

    horizontal stepper validation enabled

--- src/components/Request/PersonslInfoStepper.js
+++ src/components/Request/PersonslInfoStepper.js
@@ -64,16 +64,16 @@ const PersonalInfoStepper = forwardRef((props, ref) => {
     props.Next();
   };
   const handleNext = () => {
-    // if (activeStep == 0 || activeStep == 1 || activeStep == 3) {
-    //   childRef.current.saveData();
-    //   const isVilid = childRef.current.Validate();
-    //   if (isVilid == true) {
-    //     setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    //   }
-    // } else {
+    if (activeStep == 0 || activeStep == 1 || activeStep == 3) {
+      childRef.current.saveData();
+      const isVilid = childRef.current.Validate();
+      if (isVilid == true) {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+      }
+    } else {
       childRef.current.saveData();
       setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    //}
+    }
   };
 
   const handleBack = () => {
