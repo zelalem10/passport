2942d5a330882c34dd0eaa0ec215ed73fc8b8d75
Zelalem <zelalembe@ethiopianairlines.com>
2020-10-12 12:48:24 +0300

    add txt in instruction page

--- src/components/Payment/Responses/InstructionPage.js
+++ src/components/Payment/Responses/InstructionPage.js
@@ -43,15 +43,18 @@ function InstructionPage() {
                 : null  :null}
 
               <p class="">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio iusto, perspiciatis mollitia natus harum?
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio iusto, perspiciatis mollitia natus harum?
+                N.B : Please ensure that you make the extract payment within 2 hours after making the request. 
+                
 
           </p>
-
-              <p class="">
-                N.B : Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio iusto, perspiciatis mollitia natus harum?
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio iusto, perspiciatis mollitia natus harum?
-
+          <p class="">
+          If you do not get any SMS or email confirmation in 2 hours after marking the payment,  
+          Please contact us by this email address: 
+          <a href="mailto:support@ethiopianpassportservices.gov.et" class='ml-1'>
+            <strong>
+             support@ethiopianpassportservices.gov.et
+             </strong>
+             </a>
           </p>
 
             </div>
